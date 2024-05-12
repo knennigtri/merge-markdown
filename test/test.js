7d9c7eb12b43bfccf3264e23293d9fc29b56b310
Kevin Nennig <knennig213@gmail.com>
2024-05-11 21:44:11 -0400

    tests updated to new app

--- test/test.js
+++ test/test.js
@@ -2,57 +2,125 @@
 var init = require("../index.js");
 var minimist = require("minimist");
 var args = minimist(process.argv.slice(3));
+const fs = require('fs');
+const path = require('path');
+
 
 test(args.hideconsole);
 
-async function test(hideconsole){
-  var arr = [];
-
-  // Test json and yaml manifests
-  arr.push(
-    ["test/markdown/src/manifest.json", false, "", false, false],
-    ["test/markdown/src/manifest.yml", false, "", false, false]
-  );
-  // Test IO missing
-  arr.push(
-    ["test/markdown/src/manifests/manifest-noIO.yml", false, "", false, false],
-    ["test/markdown/src/manifests/manifest-noInput.yml", false, "", false, false],
-    ["test/markdown/src/manifests/manifest-noOutput.yml", false, "", false, false]
-  );
-  // Test only folders as manifest
-  arr.push(
-    ["test/markdown/src", false, "", false, false],
-    ["test/yaml-test", false, "", false, false],
-  );
-  // Test pdf and html generation
-  arr.push(
-    ["test/pdf/src/manifest.yml", false, "pdf", false, false],
-    ["test/pdf/src/manifest-noPresentation.yml", false, "pdf", false, false],
-    ["test/pdf/src/manifest.yml", false, "html", false, false],
-    ["test/pdf/src/manifest-noPresentation.yml", false, "html", false, false]
-  );
-
+async function test(hideconsole) {
   // Hide console logs
-  if(hideconsole){
-    console.log = function() {};
+  if (hideconsole) {
+    console.log = function () { };
   }
   var errors = 0;
   var errArr = [];
-  for(var i = 0; i < arr.length; i++){
-    try{ 
-      // (manifestParam, qaParam, modeParam, noLinkcheckParam, maintainAssetPaths)
-      init.merge.start(arr[i][0],arr[i][1],arr[i][2],arr[i][3],arr[i][4]);
+
+  /*
+  TEST - merging files in manifests
+ [manifestFile, qaMode, skip_linkcheck, maintainAssetPaths, presentationType, producesError] 
+ */
+  var mergeArr = [];
+  mergeArr.push( // Test different manifests
+    ["test/markdown/src/myManifest.yml", false, false, false, 'pdf', 0],
+    ["test/markdown/src/myManifest.json", false, false, false, 'pdf', 0],
+    ["test/pdf/src/myManifest.yml", false, false, false, 'pdf', 0],
+    ["test/pdf/src/manifest-noPresentation.yml", false, false, false, 'pdf', 0],
+    ["test/pdf/src/myManifest.yml", false, false, false, 'html', 0],
+    ["test/pdf/src/manifest-noPresentation.yml", false, false, false, 'html', 0]
+  );
+  mergeArr.push( // Test different params
+    ["test/markdown/src/myManifest.yml", true, false, false, 'pdf', 0],
+    ["test/markdown/src/myManifest.yml", false, true, false, 'pdf', 0],
+    ["test/markdown/src/myManifest.yml", false, false, true, 'pdf', 0],
+  )
+  mergeArr.push( // Test IO missing
+    ["test/markdown/src/manifests/manifest-noIO.yml", false, false, false, 1],
+    ["test/markdown/src/manifests/manifest-noInput.yml", false, false, false, 1],
+    ["test/markdown/src/manifests/manifest-noOutput.yml", false, false, false, 1]
+  );
+  for (var i = 0; i < mergeArr.length; i++) {
+    try {
+      // (manifestParam, qaParam, noLinkcheckParam, maintainAssetPaths)
+      console.log(mergeArr[i][0]);
+      init.merge.start(mergeArr[i][0], mergeArr[i][1], mergeArr[i][2], mergeArr[i][3]);
       var promise = new Promise(res => {
         setTimeout(() => res("Now it's done!"), 500);
       });
-      await promise; 
+      await promise;
     } catch (err) {
-      errors++;
-      errArr.push(arr[i]);
+      console.log(err)
+      if (mergeArr[i][mergeArr.length - 1]) {
+        errors++;
+        errArr.push(['==merge.start==', mergeArr[i]]);
+      }
     }
   }
+
+  /*
+ TEST - merging files in manifests
+[folderDirectory, producesError] 
+*/
+  var createArr = [];
+  createArr.push(
+    ["test/markdown/src", 0],
+    ["test/yaml-test", 0],
+    ["test/pdf", 0]
+  );
+  for (var i = 0; i < createArr.length; i++) {
+    try {
+      init.manifestUtil.createManifestFile(createArr[i][0]);
+    } catch (err) {
+      errors++;
+      if (createArr[i][createArr.length - 1]) {
+        console.log(err)
+        errArr.push(['==manifest.create==', createArr[i]]);
+      }
+    }
+  }
+
+  fs.unlink('./manifest.yml', (err) => {
+    if (err) console.error('Error deleting testing file:', err);
+  });
+
+  //TODO write tests for pdf/html output
+  //TODO write tests for docker
+
   console.error("Total errors in testing: " + errors);
-  for(var j = 0; j < errArr.length; j++){
+  for (var j = 0; j < errArr.length; j++) {
     console.error("  [" + errArr[j] + "]");
   }
+  deleteFoldersAndFiles('test/');
+}
+
+function deleteFoldersAndFiles(directoryPath) {
+  const foldersToDelete = ['merged', 'target'];
+  const filesToDelete = ['docker-compose.yml', 'Dockerfile'];
+
+  // Function to recursively delete folders and files
+  function deleteRecursively(dir) {
+    const files = fs.readdirSync(dir);
+    files.forEach(file => {
+      const filePath = path.join(dir, file);
+      const stat = fs.statSync(filePath);
+      if (stat.isDirectory()) {
+        deleteRecursively(filePath); // Recursively delete subfolders
+      } else {
+        // Check if the file matches the ones to delete
+        if (filesToDelete.includes(file)) {
+          fs.unlinkSync(filePath);
+          console.log(`Deleted file: ${filePath}`);
+        }
+      }
+    });
+
+    // Check if the current directory matches the ones to delete
+    if (foldersToDelete.includes(path.basename(dir))) {
+      fs.rmSync(dir, { recursive: true });
+      console.log(`Deleted folder: ${dir}`);
+    }
+  }
+
+  // Start recursively deleting folders and files
+  deleteRecursively(directoryPath);
 }
