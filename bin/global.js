fb9210c25d2e7449558ef455ac15a62644a97d9a
Kevin Nennig <knennig213@gmail.com>
2025-03-04 17:51:13 -0700

    Update global.js

--- bin/global.js
+++ bin/global.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
-var module = require("../src/cli.js");
+// Check if the environment is production
+const isProduction = process.env.NODE_ENV === 'production';
+
+// Conditionally require the appropriate module
+const module = isProduction ? require("../dist/cli.js") : require("../src/cli.js");
 
 module.run();
